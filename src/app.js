d458bd74a78cb0f9718cbce54ee096dbf25eb847
Manuelacostar97 <manuelacostar97@gmail.com>
2022-08-10 01:24:48 +0000

    Movi el projecto a la carpeta src

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log("Hello World");
+
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var exten = [".com", ".es"];
+var dominio = [];
+var seleccion = [];
+
+pronoun.forEach(p => {
+  adj.forEach(s => {
+    noun.forEach(n => {
+      exten.forEach(e => {
+        dominio.push(`${p}${s}${n}${e}`);
+      });
+    });
+  });
+});
+
+for (let i = 0; i < 16; i++) {
+  let i = parseInt(Math.random() * dominio.length);
+  seleccion.push(dominio[i]);
+  dominio.splice(i, 1);
+}
+
+console.log(dominio);
+console.log(seleccion);
